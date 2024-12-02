52ad16f91c20c5c19057be3f11356eda736697d2
LeBoufty <boufto@gmail.com>
2024-12-02 21:34:53 +0100

    timer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::time::{self, Duration, Instant};
 mod day01;
 mod day02;
 mod inputs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Result : {}", day02::part2(false)?);
+    let now = time::Instant::now();
+    let result = day02::part2(false)?;
+    println!("Result : {} // Time elapsed : {}µs", result, now.elapsed().as_micros());
     Ok(())
 }
